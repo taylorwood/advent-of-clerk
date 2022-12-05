62156f7a5482a1c64430e5d2d0e331431bbb9910
Taylor Wood <taylor@zephyrai.bio>
2022-12-05 08:28:42 -0600

    Add solutions for days 1 & 2

--- src/advent_of_clerk/day_01.clj
+++ src/advent_of_clerk/day_01.clj
@@ -1,3 +1,16 @@
 ;; # 🎄 Advent of Clerk: Day 1
 (ns advent-of-clerk.day-01
-  (:require [nextjournal.clerk :as clerk]))
+  (:require [clojure.java.io :as io]
+            [clojure.string :as s]))
+
+(def input
+  (slurp (io/resource "day_01.txt")))
+(def elves
+  (map s/split-lines (s/split input #"\n\n")))
+(def elf-calorie-totals
+  (for [elf-food-items elves]
+    (apply + (map parse-long elf-food-items))))
+;; part 1
+(apply max elf-calorie-totals)
+;; part 2
+(apply + (take 3 (sort #(compare %2 %1) elf-calorie-totals)))
--- src/advent_of_clerk/day_02.clj
+++ src/advent_of_clerk/day_02.clj
@@ -1,3 +1,60 @@
 ;; # 🎄 Advent of Clerk: Day 2
 (ns advent-of-clerk.day-02
-  (:require [nextjournal.clerk :as clerk]))
+  (:require [clojure.java.io :as io]
+            [clojure.string :as s]))
+
+(def input
+  (s/split-lines (slurp (io/resource "day_02.txt"))))
+
+(def move-map {"A" :rock, "B" :paper, "C" :scissors
+               "X" :rock, "Y" :paper, "Z" :scissors})
+(def score-map {:rock 1 :paper 2 :scissors 3})
+
+(defn move-history [move-map]
+  (for [line input
+        :let [[them _ us] line]]
+    [(move-map (str them)) (move-map (str us))]))
+
+(move-history move-map)
+
+(def win-map
+  {:rock :scissors
+   :scissors :paper
+   :paper :rock})
+
+(defn move-score [x y]
+  (let [x-win? (= (win-map x) y)
+        x-score (score-map x)
+        y-win? (= (win-map y) x)
+        y-score (score-map y)]
+    (cond
+      (and (not x-win?) (not y-win?)) [(+ 3 x-score) (+ 3 y-score)]
+      x-win? [(+ 6 x-score) y-score]
+      y-win? [x-score (+ 6 y-score)])))
+
+(move-score :rock :paper)
+(move-score :paper :rock)
+(move-score :scissors :scissors)
+
+(def round-scores
+  (map #(apply move-score %) (move-history move-map)))
+
+;; part 1
+
+(apply + (map second round-scores))
+
+(def move-map-2 (assoc move-map "X" :lose, "Y" :draw, "Z" :win))
+(def lose-map (clojure.set/map-invert win-map))
+
+(def strategic-moves
+  (for [[them us] (move-history move-map-2)]
+    [them (case us
+            :win (lose-map them)
+            :lose (win-map them)
+            :draw them)]))
+
+(def second-round-scores
+  (map #(apply move-score %) strategic-moves))
+
+;; part 2
+(apply + (map second second-round-scores))
